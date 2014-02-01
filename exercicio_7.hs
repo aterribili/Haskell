2b15d342dbbf71300443ff5ed1b28e153c6a13c4
aterribili <abner.terribili@gmail.com>
2014-02-01 21:44:22 -0200

    Inclusão Exercício 7

--- exercicio_7.hs
+++ exercicio_7.hs
@@ -2,7 +2,7 @@
 -- Hackeando, Haskell: Lazy Evaluation e Listas
 
 -- Pense em uma opção para 'burlar' a criação de uma lista com dados homogêneos.
--- Se você leu a respeito de Tuplas: , saberá como :)
+-- Se você leu a respeito de Tuplas: http://aterribili.blogspot.com.br/2014/01/haskell-tipos-compostos-com-tuplas.html , saberá como :)
 
 lista_heterogenea = [(1, "Haskell"), (2, "Win")]
 
