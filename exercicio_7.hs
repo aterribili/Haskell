d6593270994870446bd0637a85884d804a52a833
Abner Terribili <abner.terribili@gmail.com>
2014-11-21 15:06:15 -0200

    Update exercicio_7.hs
    Removendo dados incorretos

--- exercicio_7.hs
+++ exercicio_7.hs
@@ -1,7 +1,6 @@
 -- Abner Terribili
 -- Hackeando, Haskell: Lazy Evaluation e Listas
 
--- Pense em uma opção para 'burlar' a criação de uma lista com dados homogêneos.
 -- Se você leu a respeito de Tuplas: http://aterribili.blogspot.com.br/2014/01/haskell-tipos-compostos-com-tuplas.html , saberá como :)
 
 lista_heterogenea = [(1, "Haskell"), (2, "Win")]
